e4d39edcabbfd5a802d5a6f1b4e61f40f34f6ece
daichi hoshina <hoshina0713@yahoo.co.jp>
2021-09-07 00:21:59 +0900

    修正

--- backend/domain/Users.go
+++ backend/domain/Users.go
@@ -1,7 +1,6 @@
 package domain
 
 import (
-	"time"
 
 	"gorm.io/gorm"
 )
@@ -15,16 +14,16 @@ type User struct {
 	ID        int       `json:"id"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email" gorm:"unique"`
-	CreatedAt time.Time `json:"created_at"`
-	Image     string    `json:"image"`
-	Password  []byte    `json:"password"`
+	// CreatedAt time.Time `json:"created_at"`
+	// Image     string    `json:"image"`
+	// Password  []byte    `json:"password"`
 	// Presentations []Presentation `gorm:"foreignKey:UserID"`
 	// Likes         []Like         `gorm:"foreignKey:UserID"`
 }
 
 type UserForGet struct {
 	ID    int    `json:"id"`
-	Name  string `json:"screenName"`
+	Name  string `json:"Name"`
 	Email string `json:"email"`
 }
 
