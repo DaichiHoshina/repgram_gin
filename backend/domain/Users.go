5ac0cde374c0e07e2681e08fd6906bb156a0e6df
daichi hoshina <hoshina0713@yahoo.co.jp>
2021-09-08 23:32:18 +0900

    とりあえず値は取得出来るようになった

--- backend/domain/Users.go
+++ backend/domain/Users.go
@@ -1,21 +1,11 @@
 package domain
 
-import (
-
-	"gorm.io/gorm"
-)
-
-type Users struct {
-	User User `json:"users"`
-}
-
 type User struct {
-	gorm.Model
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email" gorm:"unique"`
-	// CreatedAt time.Time `json:"created_at"`
-	// Image     string    `json:"image"`
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string    `json:"email" gorm:"unique"`
+	// CedAt timeme `json:"created_at"`
+	// Ima   str    `json:"image"`
 	// Password  []byte    `json:"password"`
 	// Presentations []Presentation `gorm:"foreignKey:UserID"`
 	// Likes         []Like         `gorm:"foreignKey:UserID"`
