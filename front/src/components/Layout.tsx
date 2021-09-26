63f65b7915ad996944d9d73e791c7ba2518d8c23
daichi hoshina <hoshina0713@yahoo.co.jp>
2021-09-26 15:13:09 +0900

    ロゴを設定

--- front/src/components/Layout.tsx
+++ front/src/components/Layout.tsx
@@ -45,13 +45,13 @@ export const Layout: React.FC<LayoutProps> = ({ children, title }) => {
               <Link href="/presentations" color="inherit" underline="none">
                 {/* タイトルロゴ */}
                 <Typography variant="h6" color="inherit" className="flex-grow">
-                  Repgram
+                  <img src="/img/logo.png" className="object-contain h-12" />
                 </Typography>
               </Link>
             </Toolbar>
           </div>
 
-          <div className="ml-auto mt-5 mr-2">
+          <div className="ml-auto mt-2 mr-2">
             <Typography component="h2" variant="h6" color="inherit">
               <SearchField />
             </Typography>
