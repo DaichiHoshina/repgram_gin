f102e40994e51956be7e3bf4c993295b788bbed7
daichi hoshina <hoshina0713@yahoo.co.jp>
2021-10-10 20:34:31 +0900

    gracefuly shutdown実装

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,47 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/DaichiHoshina/repgram_gin/backend/infrastructure"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	db := infrastructure.NewDB()
 
 	r := infrastructure.NewRouting(db)
+
+	srv := &http.Server{
+		Addr:    ":3001",
+		Handler: r.Gin,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	stop()
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 
 	r.Run()
 }
